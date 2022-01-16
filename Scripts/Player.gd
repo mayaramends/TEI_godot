62dca829616e6e2d788a12a3928379d3a46b2e69
Mayara Mendes <mayara.mendes.mdc@gmail.com>
2022-01-15 22:56:51 -0300

    script e animacoes

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,15 +4,45 @@ var velocity = Vector2.ZERO #movimentacao eixos X e Y
 var move_speed = 480 #velocidade
 var gravity = 1200 #gravidade
 var jump_force = -720 #eixo Y e invertido
+var is_grounded = false
+onready var raycasts = $raycasts
 
 func _physics_process(delta:float) -> void:
 	velocity.y += gravity * delta
 	
+	_get_input()
+	
+	velocity = move_and_slide(velocity)
+	
+	is_grounded = _check_is_ground()
+	
+	_set_animation()
+
+func _get_input():
+	velocity.x = 0
 	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2)
 	
-	if Input.is_action_pressed("jump"):
+	if move_direction != 0:
+		$texture.scale.x = move_direction
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump") and is_grounded:
 		velocity.y = jump_force / 2
+		
+func _check_is_ground():
+	for raycast in raycasts.get_children():
+		if raycast.is_colliding():
+			return true
+			
+	return false
+
+func _set_animation():
+	var anim = "idle"
+	if !is_grounded:
+		anim = "jump"
+	elif velocity.x != 0:
+		anim = "run"
 	
-	velocity.x = move_speed * move_direction
-	
-	move_and_slide(velocity)
+	if $anim.assigned_animation != anim:
+		$anim.play(anim)
