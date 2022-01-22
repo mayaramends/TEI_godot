c1ee78ceaa44cdb92de00f16e5e9be5cd182d24e
Mayara Mendes <mayara.mendes.mdc@gmail.com>
2022-01-22 17:28:27 -0300

    criacao do inimigo, danos ao inimigo e ao player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,6 +5,13 @@ var move_speed = 480 #velocidade
 var gravity = 1200 #gravidade
 var jump_force = -720 #eixo Y e invertido
 var is_grounded = false
+var health = 3
+var hurted = false
+
+# direcao e intensidade do empurrao
+var knockback_dir = 1
+var knockback_int = 500
+
 onready var raycasts = $raycasts
 
 func _physics_process(delta:float) -> void:
@@ -25,6 +32,7 @@ func _get_input():
 	
 	if move_direction != 0:
 		$texture.scale.x = move_direction
+		knockback_dir = move_direction
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump") and is_grounded:
@@ -44,5 +52,32 @@ func _set_animation():
 	elif velocity.x != 0:
 		anim = "run"
 	
+	if velocity.y > 0 and !is_grounded:
+		anim = "fall"
+	
+	if hurted == true:
+		anim = "hit"
+	
 	if $anim.assigned_animation != anim:
 		$anim.play(anim)
+
+func knockback():
+	velocity.x = -knockback_dir * knockback_int
+	velocity = move_and_slide(velocity)
+
+func _on_hurtbox_body_entered(body):
+	health -= 1
+	
+	hurted = true
+	knockback()
+	
+	get_node("hurtbox/collision").set_deferred("disabled", true)
+	yield(get_tree().create_timer(0.5), "timeout")
+	get_node("hurtbox/collision").set_deferred("disabled", false)
+	
+	hurted = false
+	
+	if health < 1:
+		queue_free()
+		get_tree().reload_current_scene()
+		
